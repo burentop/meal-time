9dec07da9a8450b4789ac8450480c3db4c305a0f
Brent Perry <contact@brentallan.me>
2019-01-20 17:07:05 -0600

    add breakfast question.

--- MealTime.cs
+++ MealTime.cs
@@ -4,6 +4,8 @@ class MealTime
 {
     static void Main()
     {
-        
+        Console.WriteLine("What did you eat for breakfast?");
+        string yourBreakfast = Console.ReadLine();
+        Console.WriteLine("You had " + yourBreakfast + " for breakfast.");
     }
 }
